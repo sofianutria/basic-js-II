8a786d04816b2e72ac94b893f950960ff199ea15
Sofía Santos García <sofiasantos@MacBook-Air-de-Sofia.local>
2025-04-10 13:41:05 +0200

    Activities 1-5

--- script.js
+++ script.js
@@ -10,26 +10,50 @@ console.log('Hola soy tu consola y juntas vamos a aprender Javascript')
 
 //Ejercicio 1: Escribe un condicional if/else que imprima en la consola 'Eres mayor de edad' cuando la constante age tenga el valor correspondiente, si no es así, debe imprimir 'No eres aún mayor de edad'
 
-const age = 18
+const age = 12;
 //Escribe tu código aquí
-
+if (age<18){
+    console.log('No eres aún mayor de edad');
+} else {
+    console.log('Eres mayor de edad');
+}
 
 //Ejercicio 2: Cambia el valor de age a 12 para mirar el resultado del ejercicio anterior.
-
+//No se puede crear una nueva variable con el nombre de age por lo que he cambiado el número de age a 12 directamente.
 
 //Ejercicio 3: Escribe un condicional if/else que lea la constante pet y si es un perro, que imprima en la consola "Tengo un perro", si es un gato, que imprima en la consola "Tengo un gato", si es un pájaro, que imprima en la consola "Tengo un pájaro" y si no es ninguno de los 3 que imprima "No tengo una mascota convencional"
 
-const pet = "perro";
+const pet = "serpiente";
 //Escribe tu código aquí
-
+if (pet=== "perro"){
+    console.log('Tengo un perro');
+} else if (pet=== "gato"){
+    console.log('Tengo un gato');
+} else if (pet=== "pájaro"){
+    console.log('Tengo un pájaro');
+} else {
+    console.log('No tengo una mascota convencional');
+}
 
 //Ejercicio 4: cambia el valor de pet a "serpiente" y mira el resultado en consola para comprobar que tu código funciona bien.
-
+//No se puede crear una nueva variable con el nombre de pet por lo que he cambiado el nombre de pet a serpiente directamente.
 
 //Ejercicio 5: Haz el mismo ejercicio 3, pero con el condicional switch.
 
 //Escribe tu código aquí
-
+switch (pet){
+    case "perro":
+        console.log('Tengo un perro');
+    break;
+    case "gato":
+        console.log('Tengo un gato');
+    break;
+    case "pájaro":
+        console.log('Tengo un pájaro');
+    break;
+    default: 
+        console.log('No tengo una mascota convencional');
+}
 
 //Ejercicio 6: Usando un operador ternario, escribe un programa que lea la constante weather e imprima en consola si es soleado "Me vestiré con un vestido" y si no es así que imprima "Me vestiré con pantalón"
 
